a30d71a492dd91fe906b2fe71fced159569d4380
Gathaspar66 <lukasiewiczkacper@outlook.com>
2025-09-19 23:56:56 +0200

    Added pickup

---
+++ Assets/TESTTESTTEST/Pickup.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public float destroyRadius = 10f;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, destroyRadius);
+            foreach (Collider hit in hitColliders)
+            {
+                if (hit.CompareTag("Enemy"))
+                {
+                    Destroy(hit.gameObject);
+                }
+            }
+
+
+
+            Destroy(gameObject);
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, destroyRadius);
+    }
+}
