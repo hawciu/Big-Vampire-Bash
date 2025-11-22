db626e1ff395664469756804aa48fd29ec515a02
Gathaspar66 <lukasiewiczkacper@outlook.com>
2025-11-22 22:04:16 +0100

    Added meteor

---
+++ Assets/Scripts/Pickups/PickupMeteorProjectile1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PickupMeteorProjectile1 : MonoBehaviour
+{
+    public float impactRadius = 4f;
+    public LayerMask enemyLayer;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, impactRadius, enemyLayer);
+        foreach (Collider hit in hits)
+        {
+            if (hit.TryGetComponent<EnemySimple>(out EnemySimple enemy))
+            {
+                enemy.Damage();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, impactRadius);
+    }
+}
