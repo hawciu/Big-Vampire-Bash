a3acb49f95c300fbb59d1f73049130d8099b7650
Gathaspar66 <lukasiewiczkacper@outlook.com>
2025-11-24 21:34:03 +0100

    Pick up effect.

---
+++ Assets/TESTTESTTEST/EffectSequence3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EffectSequenceSimultaneous : MonoBehaviour
+{
+    public ParticleSystem effect1;
+    public ParticleSystem effect2;
+    public ParticleSystem effect3;
+
+    private void Start()
+    {
+        _ = StartCoroutine(RunSequence());
+    }
+
+    private IEnumerator RunSequence()
+    {
+        effect1.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        effect2.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        effect3.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
+        effect1.Play();
+
+        yield return new WaitUntil(() => !effect1.IsAlive());
+
+        effect2.Play();
+        effect3.Play();
+    }
+}
