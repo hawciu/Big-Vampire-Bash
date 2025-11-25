e22d6462e56f8257e22aed6962f674a6712bc930
Gathaspar66 <lukasiewiczkacper@outlook.com>
2025-11-26 00:17:04 +0100

    Initial version of HUB

--- Assets/Scripts/Managers/HubManager.cs
+++ Assets/Scripts/Managers/HubManager.cs
@@ -1,25 +1,70 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HubManager : MonoBehaviour
 {
     public static HubManager instance;
+    public Transform previewParent;
+
+    public List<GameObject> playerPrefabs;
+
+    private int currentIndex = 0;
+    private GameObject currentPreview;
 
     private void Awake()
     {
         instance = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        ShowPreview(currentIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Next()
     {
-        
+        currentIndex++;
+        if (currentIndex >= playerPrefabs.Count)
+        {
+            currentIndex = 0;
+        }
+
+        ShowPreview(currentIndex);
+    }
+
+    public void Previous()
+    {
+        currentIndex--;
+        if (currentIndex < 0)
+        {
+            currentIndex = playerPrefabs.Count - 1;
+        }
+
+        ShowPreview(currentIndex);
+    }
+
+    private void ShowPreview(int index)
+    {
+        if (currentPreview != null)
+        {
+            Destroy(currentPreview);
+        }
+
+        currentPreview = Instantiate(playerPrefabs[index], previewParent);
+
+        currentPreview.transform.localPosition = Vector3.zero;
+        currentPreview.transform.localRotation = Quaternion.identity;
+        currentPreview.transform.localScale = Vector3.one;
+    }
+
+    public int GetSelectedIndex()
+    {
+        return currentIndex;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
