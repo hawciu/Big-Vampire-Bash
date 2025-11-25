615011200cca0136beb21005f066bc033f2389f9
hawciu <hawciu@gmail.com>
2025-11-25 18:37:38 +0100

    portal probably completed

--- Assets/TESTTESTTEST/PortalScript.cs
+++ Assets/TESTTESTTEST/PortalScript.cs
@@ -1,61 +1,116 @@
 using System;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public enum PortalState
 {
     NONE,
+    MOVE_CAMERA_TO_PORTAL,
     CIRCLING,
     OPENING,
     TELEPORTING,
-    CLOSING
+    CLOSING,
+    MOVE_CAMERA_BACK,
 }
 
 public class PortalScript : MonoBehaviour
 {
+    public Camera mainCamera;
+
+    public Camera cinematicCamera;
     public GameObject center;
     public GameObject circle;
     public GameObject guy;
     float speed = 1f;
     float duration = 0.2f;
     float openingCounter = 0f;
-    float waitTimeAfterOpening = 1f;
+    float waitTimeAfterOpening = 0.5f;
+    float cameraTransitionCounter = 0f;
+    Vector3 cameraTargetPosition;
+    Quaternion cameraTargetRotation;
+    Vector3 startCameraTargetPosition;
+    Quaternion startCameraTargetRotation;
+    public GameObject portalCameraTarget;
     Vector3 circleTargetScale = new Vector3(4,0,4);
     float characterTargetHeight;
     public ParticleSystem sparks;
     public ParticleSystem portalLines;
     public ParticleSystem end;
-
-    public bool go = true;
+    public bool enteringPortal = false;
+    bool rotate = false;
+    bool skipMoveTo = false;
 
     PortalState state = PortalState.NONE;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        state = PortalState.CIRCLING;
-        sparks.Play();
-        if(guy != null)
-        {
-            characterTargetHeight = guy.transform.position.y;
-        }
-        else
-        {
-            Debug.LogWarning("START portal character guy not set");
-        }
+        //to remove upon implementation
+        mainCamera = Camera.main;
+        SetupPortal(guy, mainCamera);
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdatePortalState();
-        if(state != PortalState.NONE) center.transform.Rotate(0, 100 * Time.deltaTime * speed, 0);
+    }
+
+    public void SetupPortal(GameObject guyTarget, Camera targetCamera, bool enteringPorta = true, bool skipMoveTo = false)
+    {
+        guy = guyTarget;
+        mainCamera = targetCamera;
+        this.skipMoveTo = skipMoveTo;
+        if (enteringPortal)
+        {
+            cinematicCamera.transform.position = mainCamera.transform.position;
+            cinematicCamera.transform.rotation = mainCamera.transform.rotation;
+            cameraTargetPosition = portalCameraTarget.transform.position;
+            cameraTargetRotation = portalCameraTarget.transform.rotation;
+        }
+        else
+        {
+            cinematicCamera.transform.position = portalCameraTarget.transform.position;
+            cinematicCamera.transform.rotation = portalCameraTarget.transform.rotation;
+            cameraTargetPosition = mainCamera.transform.position;
+            cameraTargetRotation = mainCamera.transform.rotation;
+        }
+        startCameraTargetPosition = portalCameraTarget.transform.position;
+        startCameraTargetRotation = portalCameraTarget.transform.rotation;
+        ActivatePortal();
+    }
+
+    public void ActivatePortal()
+    {
+        mainCamera.enabled = false;
+        cinematicCamera.enabled = true;
+        rotate = true;
+        sparks.Play();
+        if (guy != null)
+        {
+            characterTargetHeight = 1.4f;
+        }
+        else
+        {
+            Debug.LogWarning("START portal character guy not set");
+        }
+        state = PortalState.MOVE_CAMERA_TO_PORTAL;
     }
 
     private void UpdatePortalState()
     {
+        if (rotate) center.transform.Rotate(0, 100 * Time.deltaTime * speed, 0);
         switch (state)
         {
             case PortalState.NONE:
+                break;
+
+            case PortalState.MOVE_CAMERA_TO_PORTAL:
+                if (skipMoveTo || MoveCameraLerp())
+                {
+                    state = PortalState.CIRCLING;
+                }
                 break;
 
             case PortalState.CIRCLING:
@@ -73,7 +128,7 @@ public class PortalScript : MonoBehaviour
                 if (openingCounter < duration)
                 {
                     openingCounter += Time.deltaTime;
-                    circle.transform.localScale = circleTargetScale * openingCounter / duration + new Vector3(0, 0.01f, 0);
+                    circle.transform.localScale = circleTargetScale * openingCounter / duration;
                 }
                 else
                 {
@@ -85,6 +140,7 @@ public class PortalScript : MonoBehaviour
             case PortalState.TELEPORTING:
                 waitTimeAfterOpening -= Time.deltaTime;
                 if (waitTimeAfterOpening > 0) return;
+
                 if (characterTargetHeight > -2.5f)
                 {
                     characterTargetHeight -= Time.deltaTime * 5;
@@ -108,8 +164,29 @@ public class PortalScript : MonoBehaviour
                 circle.SetActive(false);
                 end.Play();
                 center.SetActive(false);
-                state = PortalState.NONE;
+                state = PortalState.MOVE_CAMERA_BACK;
+                break;
+
+            case PortalState.MOVE_CAMERA_BACK:
+                if (MoveCameraLerp())
+                {
+                    state = PortalState.NONE;
+                    PortalActionCompleted();
+                }
                 break;
         }
+    }
+
+    bool MoveCameraLerp()
+    {
+        cameraTransitionCounter += Time.deltaTime;
+        cinematicCamera.transform.position = Vector3.Lerp(startCameraTargetPosition, cameraTargetPosition, cameraTransitionCounter);
+        cinematicCamera.transform.rotation = Quaternion.Lerp(startCameraTargetRotation, cameraTargetRotation, cameraTransitionCounter);
+        return cameraTransitionCounter >= 1;
+    }
+
+    void PortalActionCompleted()
+    {
+
     }
 }
